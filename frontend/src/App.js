0d209a57782191818a441eb1ce3243df8d1d8732
Dhruv9979 <starb9979@gmail.com>
2020-12-02 21:45:15 -0500

    added the homepage and product page and components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header.component';
+import Footer from './components/Footer.component';
+import HomePage from './pages/HomePage.component';
+import ProductPage from './pages/ProductPage.component.jsx';
 
 const App = () => {
     return (
-        <>
-            <h1>Welcome to ProShop</h1>
-        </>
+        <Router>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <Route path="/" component={HomePage} exact />
+                    <Route path="/product/:id" component={ProductPage} />
+                </Container>
+            </main>
+            <Footer />
+        </Router>
     );
 };
 
